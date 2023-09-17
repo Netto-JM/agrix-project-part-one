8210d6283161259fe867b90cbec8fb3054092f2b
NettoJM <nettojm86@gmail.com>
2023-09-17 13:56:54 -0300

    Adds DTO for Farm

---
+++ src/main/java/com/betrybe/agrix/controllers/dto/FarmDTO.java
@@ -0,0 +1,12 @@
+package com.betrybe.agrix.controllers.dto;
+
+import com.betrybe.agrix.models.entities.Farm;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record FarmDTO(Long id, String name, @JsonProperty("planted_area") Double plantedArea) {
+
+  public Farm toFarm() {
+    return new Farm(id, name, plantedArea, null);
+  }
+
+}
