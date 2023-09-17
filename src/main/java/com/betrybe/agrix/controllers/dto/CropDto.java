9880af2ddcee93b5a367a8128a21d438f88632e5
NettoJM <nettojm86@gmail.com>
2023-09-17 14:15:06 -0300

    Adds repositories for Crop and Farm

--- src/main/java/com/betrybe/agrix/controllers/dto/CropDto.java
+++ src/main/java/com/betrybe/agrix/controllers/dto/CropDto.java
@@ -2,9 +2,9 @@ package com.betrybe.agrix.controllers.dto;
 
 import com.betrybe.agrix.models.entities.Crop;
 
-public record CropDTO(Long id, String name, Double size) {
+public record CropDto(Long id, String name, Double size) {
 
-  public CropDTO {
+  public CropDto {
     if (size < 0) {
       throw new IllegalArgumentException("Size must be greater than zero");
     }
