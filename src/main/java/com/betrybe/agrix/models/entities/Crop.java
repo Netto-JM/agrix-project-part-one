0cae2caf31a203771071cec09d3e0c549d0ae888
NettoJM <nettojm86@gmail.com>
2023-09-16 23:36:24 -0300

    Adds JavaDocs

--- src/main/java/com/betrybe/agrix/models/entities/Crop.java
+++ src/main/java/com/betrybe/agrix/models/entities/Crop.java
@@ -8,24 +8,50 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+/**
+ * Represents a crop entity.
+ */
 @Entity
 @Table(name = "crop")
 public class Crop {
 
+  /**
+   * The unique identifier of the crop.
+   */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  /**
+   * The name of the crop.
+   */
   private String name;
 
+  /**
+   * The size of the crop.
+   */
   private Double size;
 
+  /**
+   * The farm to which this crop belongs.
+   */
   @ManyToOne
   @JoinTable(name = "farm_id")
   private Farm farm;
 
+  /**
+   * Default constructor for Crop.
+   */
   public Crop() {}
 
+  /**
+   * Parameterized constructor for Crop.
+   *
+   * @param id   The unique identifier of the crop.
+   * @param name The name of the crop.
+   * @param size The size of the crop.
+   * @param farm The farm to which this crop belongs.
+   */
   public Crop(Long id, String name, Double size, Farm farm) {
     this.id = id;
     this.name = name;
@@ -33,34 +59,74 @@ public class Crop {
     this.farm = farm;
   }
 
+  /**
+   * Get the unique identifier of the crop.
+   *
+   * @return The unique identifier of the crop.
+   */
   public Long getId() {
     return id;
   }
 
+  /**
+   * Set the unique identifier of the crop.
+   *
+   * @param id The unique identifier of the crop.
+   */
   public void setId(Long id) {
     this.id = id;
   }
 
+  /**
+   * Get the name of the crop.
+   *
+   * @return The name of the crop.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Set the name of the crop.
+   *
+   * @param name The name of the crop.
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Get the size of the crop.
+   *
+   * @return The size of the crop.
+   */
   public Double getSize() {
     return size;
   }
 
+  /**
+   * Set the size of the crop.
+   *
+   * @param size The size of the crop.
+   */
   public void setSize(Double size) {
     this.size = size;
   }
 
+  /**
+   * Get the farm to which this crop belongs.
+   *
+   * @return The farm to which this crop belongs.
+   */
   public Farm getFarm() {
     return farm;
   }
 
+  /**
+   * Set the farm to which this crop belongs.
+   *
+   * @param farm The farm to which this crop belongs.
+   */
   public void setFarm(Farm farm) {
     this.farm = farm;
   }
--- src/main/java/com/betrybe/agrix/models/entities/Farm.java
+++ src/main/java/com/betrybe/agrix/models/entities/Farm.java
@@ -9,6 +9,9 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
 
+/**
+ * Represents a farm entity.
+ */
 @Table(name = "farm")
 public class Farm {
 
@@ -25,8 +28,19 @@ public class Farm {
   @JsonIgnore
   private List<Crop> crops;
 
+  /**
+   * Default constructor for the Farm entity.
+   */
   public Farm() {}
 
+  /**
+   * Parameterized constructor for the Farm entity.
+   *
+   * @param id          The unique identifier of the farm.
+   * @param name        The name of the farm.
+   * @param plantedArea The total planted area of the farm.
+   * @param crops       The list of crops associated with the farm.
+   */
   public Farm(Long id, String name, Double plantedArea, List<Crop> crops) {
     this.id = id;
     this.name = name;
@@ -34,34 +48,74 @@ public class Farm {
     this.crops = crops;
   }
 
+  /**
+   * Get the unique identifier of the farm.
+   *
+   * @return The farm's ID.
+   */
   public Long getId() {
     return id;
   }
 
+  /**
+   * Set the unique identifier of the farm.
+   *
+   * @param id The farm's ID.
+   */
   public void setId(Long id) {
     this.id = id;
   }
 
+  /**
+   * Get the name of the farm.
+   *
+   * @return The farm's name.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Set the name of the farm.
+   *
+   * @param name The farm's name.
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Get the total planted area of the farm.
+   *
+   * @return The total planted area in acres.
+   */
   public Double getPlantedArea() {
     return plantedArea;
   }
 
+  /**
+   * Set the total planted area of the farm.
+   *
+   * @param plantedArea The total planted area in acres.
+   */
   public void setPlantedArea(Double plantedArea) {
     this.plantedArea = plantedArea;
   }
 
+  /**
+   * Get the list of crops associated with the farm.
+   *
+   * @return A list of Crop objects.
+   */
   public List<Crop> getCrops() {
     return crops;
   }
 
+  /**
+   * Set the list of crops associated with the farm.
+   *
+   * @param crops A list of Crop objects.
+   */
   public void setCrops(List<Crop> crops) {
     this.crops = crops;
   }
